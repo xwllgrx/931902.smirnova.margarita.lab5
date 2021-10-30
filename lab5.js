5403df403abcf2804f0f9c4a1077bbcd456a18b5
xwllgrx <81639171+xwllgrx@users.noreply.github.com>
2021-10-30 22:40:08 +0700

    lab5

---
+++ lab5.js
@@ -0,0 +1,23 @@
+function show1() {
+	document.getElementsByClassName("back")[0].style.display = 'block';
+	document.getElementById('news1').style.opacity = '0.3';
+	document.getElementById('news2').style.opacity = '0.3';
+	document.getElementById('news3').style.opacity = '0.3';
+	document.getElementById('descrip1').style.display = 'block';
+}
+
+function show2() {
+	document.getElementsByClassName("back")[0].style.display = 'block';
+	document.getElementById('news2').style.opacity = '0.3';
+	document.getElementById('news1').style.opacity = '0.3';
+	document.getElementById('news3').style.opacity = '0.3';
+	document.getElementById('descrip2').style.display = 'block';
+}
+
+function show3() {
+	document.getElementsByClassName("back")[0].style.display = 'block';
+	document.getElementById('news1').style.opacity = '0.3';
+	document.getElementById('news2').style.opacity = '0.3';
+	document.getElementById('news3').style.opacity = '0.3';
+	document.getElementById('descrip3').style.display = 'block';
+}
